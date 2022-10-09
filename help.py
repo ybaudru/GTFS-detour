0241ebd949130ea490ba871b902d31144d2d476a
ybaudru <103264413+ybaudru@users.noreply.github.com>
2022-10-09 17:48:57 -0400

    minor update

--- help.py
+++
@@ -1,8 +0,0 @@
-
-import configparser
-
-# Method to read config file settings
-def read_config(Config_File):
-    config = configparser.ConfigParser()
-    config.read(Config_File)
-    return config
